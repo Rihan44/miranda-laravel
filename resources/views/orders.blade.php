e4c8ae22a16b4349f2e52bbae0a30eb586571182
Rihan44 <asmuela_bustamante@hotmail.es>
2023-12-14 00:45:30 +0100

    [Update] forms visual done

--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -48,7 +48,7 @@
                                     <td class="px-6 py-4">
                                         <button type="button" class="px-3 py-2 text-xs font-medium text-center text-white bg-green-700 rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
                                             <a href="/edit_order/{{$order->id}}">                           
-                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
                                                     <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path>
                                                     <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd"></path>
                                                 </svg>
--- resources/views/room_service.blade.php
+++ resources/views/room_service.blade.php
@@ -9,8 +9,28 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    Haz tu pedido
-                   
+                    <h2 class="text-4xl font-bold dark:text-dark">Do your Order</h2>
+                    <form class="max-w-sm mx-auto" method="POST" action="{{ route('orders.order_request') }}">
+                        <div class="mb-5">
+                            <label for="type" class="block mb-2 text-sm font-medium text-white-900 dark:text-dark">Order Type</label>
+                            <input type="text" id="type" name="type" class="shadow-sm bg-white-50 border border-white-300 text-dark-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white-700 dark:border-white-600 dark:placeholder-dark-400 dark:text-dark dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="Food.." required>
+                        </div>
+                        <div class="mb-5">
+                            <label for="description" class="block mb-2 text-sm font-medium text-dark-900 dark:text-dark">Order Description</label>
+                            <input type="text" id="description" name="description" class="shadow-sm bg-white-50 border border-white-300 text-dark-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white-700 dark:border-white-600 dark:placeholder-white-400 dark:text-dark dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="I want the dinner.." required>
+                        </div>
+                        <input type="hidden" name="user_id" value="{{Auth::user()->id}}">
+                        <div class="mb-7">
+                            <label for="room" class="block mb-2 text-sm font-medium text-gray-900 dark:text-dark">Select the Room</label>
+                            <select id="room" name="room_id" class="bg-white-50 border border-white-300 text-white-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white-700 dark:border-white-600 dark:placeholder-dark-400 dark:text-dark dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                <option selected>Choose a Room</option>
+                                @foreach ($orders as $order)
+                                    <option value="{{$order->room->id}}">{{$order->room->room_number}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                    </form>
                 </div>
             </div>
         </div>
