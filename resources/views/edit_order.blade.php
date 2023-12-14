427301200dc9382116e2c6b6ecac407c07ac43ac
Rihan44 <asmuela_bustamante@hotmail.es>
2023-12-14 13:13:54 +0100

    [Update] form updated form done

--- resources/views/edit_order.blade.php
+++ resources/views/edit_order.blade.php
@@ -11,6 +11,7 @@
                 <div class="p-6 text-gray-900">
                     <h2 class="text-4xl font-bold dark:text-dark">Edit your Order</h2>
                     <form class="max-w-sm mx-auto" method="POST">
+                        @csrf
                         @foreach ($order as $or)
                             <div class="mb-5">
                                 <label for="type" class="block mb-2 text-sm font-medium text-white-900 dark:text-dark">Order Type</label>
@@ -21,14 +22,15 @@
                                 <input type="text" id="description" name="description" class="shadow-sm bg-white-50 border border-white-300 text-dark-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white-700 dark:border-white-600 dark:placeholder-white-400 dark:text-dark dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" value="{{$or->description}}" required>
                             </div>
                             <input type="hidden" name="user_id" value="{{Auth::user()->id}}">
+                            <input type="hidden" name="id" value="{{$or->id}}">
                             <div class="mb-7">
                                 <label for="room" class="block mb-2 text-sm font-medium text-gray-900 dark:text-dark">Select the Room</label>
                                 <select id="room" name="room_id" class="bg-white-50 border border-white-300 text-white-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white-700 dark:border-white-600 dark:placeholder-dark-400 dark:text-dark dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                     <option selected readonly>{{$or->room->room_number}}</option>
                                 </select>
                             </div>
+                            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Edit</button>
                         @endforeach
-                        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Edit</button>
                     </form>
                 </div>
             </div>
